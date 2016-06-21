c952aac460fe2a5568d2444897588e5d6159cec4
Jason Stumpf <jason.stumpf@gmail.com>
2016-06-21 11:07:17 -0700

    Greatly improved jacques demo.

--- Track.hs
+++ Track.hs
@@ -111,7 +111,23 @@ module Main (
           trackStart = [(0, signature (4,2)), (0, tempo 120)]
           trackEnd = [(0, end)]
 
-  jacques :: Track
-  jacques = [(1, map (:[]) [60,62,64,60,60,62,64,60])]
+  jacques0 = [(2,[[60],[62],[64],[60]])]
+  jacques1 = [(2,[[64],[65]]),(4,[[67]])]
+  jacques2 = [(1,[[67],[69],[67],[65]]),(2,[[64],[60]])]
+  jacques3 = [(2,[[60],[55]]),(4,[[60]])]
 
-  main = B.putStr (runPut $ output (trackToMidi 0 0 16 jacques))
+  jacques
+    = t0
+    ++ (t0 `combine` t1)
+    ++ (t0 `combine` t1 `combine` t2)
+    ++ (t0 `combine` t1 `combine` t2 `combine` t3)
+    ++ (t1 `combine` t2 `combine` t3)
+    ++ (t2 `combine` t3)
+    ++ t3
+    where
+      t0 = trackToMidi 0 0 8 (jacques0 ++ jacques0)
+      t1 = trackToMidi 0 0 8 (jacques1 ++ jacques1)
+      t2 = trackToMidi 0 0 8 (jacques2 ++ jacques2)
+      t3 = trackToMidi 0 0 8 (jacques3 ++ jacques3)
+
+  main = B.putStr (runPut $ output jacques)
